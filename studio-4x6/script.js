847b571c4f1afc5a862ff6c5de7686861728caf2
1206enterprises-boop <1206enterprises@gmail.com>
2026-02-11 09:48:38 -0500

    Update script.js

--- studio-4x6/script.js
+++ studio-4x6/script.js
@@ -2,24 +2,28 @@ const video = document.getElementById("video");
 const startBtn = document.getElementById("startBtn");
 const takeBtn = document.getElementById("takeBtn");
 const photoLayer = document.getElementById("photoLayer");
+const templateImg = document.getElementById("templateImg");
 let count = 0;
 
+// Load frame
+templateImg.src = localStorage.getItem('frame') || '../img/4x6-template.png';
+
+// Start camera
 startBtn.onclick = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video:{width:{ideal:1080},height:{ideal:1920},facingMode:"user"}
-  });
+  const stream = await navigator.mediaDevices.getUserMedia({video:{width:{ideal:1080},height:{ideal:1920},facingMode:"user"}});
   video.srcObject = stream;
 };
 
+// Take photo
 takeBtn.onclick = () => {
   if(count>=4) return;
   const canvas = document.createElement("canvas");
   canvas.width=1080; canvas.height=1920;
-  const ctx=canvas.getContext("2d");
+  const ctx = canvas.getContext("2d");
   ctx.drawImage(video,0,0,canvas.width,canvas.height);
 
-  const img=document.createElement("img");
-  img.src=canvas.toDataURL("image/png");
+  const img = document.createElement("img");
+  img.src = canvas.toDataURL("image/png");
   img.style.left=(count*80+80)+"px";
   img.style.top="200px";
 
@@ -28,6 +32,7 @@ takeBtn.onclick = () => {
   count++;
 };
 
+// Drag & move
 function makeDraggable(el){
   let offsetX,offsetY;
   el.onmousedown=e=>{
@@ -41,12 +46,14 @@ function makeDraggable(el){
   };
 }
 
+// Download
 function downloadScrap(){
   html2canvas(document.getElementById("scrapCanvas")).then(canvas=>{
-    const link=document.createElement("a");
+    const link = document.createElement("a");
     link.download="scrap.png";
-    link.href=canvas.toDataURL();
+    link.href = canvas.toDataURL();
     link.click();
   });
 }
+
 
